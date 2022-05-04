e48abd4f21897c93729db881d230f1e42913639f
Joel Rafael Maria Brioso <maria.brioso.joel@gmail.com>
2022-05-04 14:10:42 -0700

    Agregando validacion en los modelos

--- Models/AlumnosModel.js
+++ Models/AlumnosModel.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 const conn = require("../Database/Conexion")
 //conn
+var posible_valores = ["M", "F"];
 const Alumnos = mongoose.Schema({
-     nombre:String,
-     apellido:String,
-     edad:Number,
+     nombre:{type:String, require:"El nombre es obligatorio"},
+     apellido:{type:String, require:"El apellido es obligatorio"},
+     edad:{type:Number, min:[10, "La edad no puede ser menor que 10"], max:[60, "La edad no puede ser mayor que 60"],},
      sexo:String,
      matricula:{
          nombre:String,
--- Routes/routes.js
+++ Routes/routes.js
@@ -3,10 +3,14 @@ const express = require("express"),
       AppRoute = express.Router()
       const AlumnosModel = require("../Models/AlumnosModel")
       AppRoute.get("/obtener-data",(req, res)=>{
-          AlumnosModel.find().then(resp=>{
-             res.json(resp)
-          },err=>{
-            res.status(500).json(err)
-          })
+         //AlumnosModel.find().then(resp=>{
+         //   res.json(resp)
+         //},err=>{
+         //  res.status(500).json(err)
+         //})
+         AlumnosModel.find({}, (err, docs)=>{
+          res.json(docs)
+
+         })
           });
       module.exports = AppRoute;
